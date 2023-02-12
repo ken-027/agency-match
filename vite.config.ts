6dff9cbe2b010dc5def96f0d2d48b3cdb0ed0a01
ken <82031953+ken-027@users.noreply.github.com>
2023-02-12 17:44:22 +0800

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   base: '/agency-match/',
-  build: {},
+  build: {
+    // manifest: true,
+    // emptyOutDir: false,
+  },
 })
